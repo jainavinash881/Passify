d9ae91e0c0c95a521619bc59644fcd66aa5afd32
Avinash Jain <jainavinash881@gmail.com>
2025-12-27 17:00:35 +0530

    Delete passify-source-v1.0.1 directory

--- passify-source-v1.0.1/src/manager/manager.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PasswordManager from './PasswordManager.tsx';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <PasswordManager />
-  </React.StrictMode>
-);
